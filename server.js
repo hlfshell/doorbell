0facdb2165992a9db9a480dda8d9674a2072f03d
Keith Chester <kchester@gmail.com>
2016-06-05 20:18:13 -0500

    feature: Beginning to add sms reception route

--- server.js
+++ server.js
@@ -4,8 +4,13 @@ if(fs.existsSync('./.env')) require('dotenv').config();
 
 var path = require('path');
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
+app.use(bodyParser.json());
 
+var contacts = require('./contact.json');
+
+//For serving up the image for the MMS portion
 app.get(
     '/mms/:nonce/:image',
     function(req, res, next){
@@ -31,10 +36,14 @@ app.get(
     }
 );
 
-//Temporary test endpoint
-app.get('/:image', function(req, res){
-    res.sendFile(path.join(process.env.PHOTOS_FOLDER, req.params.image));
-});
+//Accepting an SMS from twilio 
+app.get(
+    "/sms/received"
+    function(req, res, next){
+        //Is this actually from twilio?
+        
+    }
+);
 
 //Load the daemon
 var Daemon = require('./daemon.js');
@@ -45,6 +54,5 @@ var camera = new Camera();
 var daemon = new Daemon({ camera: camera });
 
 daemon.watch();
-
 
 app.listen(process.env.PORT);
